7f4618c913566b6e5f42e483cc81d44d2758e7db
Anthony Laliberte <anthony.laliberte.1@ulaval.ca>
2025-10-29 22:11:09 -0400

    added some more functions to arithmetic plugin

--- Tony.Calculator/Plugins/ArithmeticPlugin.cs
+++ Tony.Calculator/Plugins/ArithmeticPlugin.cs
@@ -4,25 +4,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MathNet.Numerics;
 
 namespace Tony.Calculator.Plugins
 {
     public class ArithmeticPlugin : IPlugin
     {
+        /*
+            to do:
+            fib 	Fibonacci number 	fib(x)
+            gcd 	Greatest common divisor 	gcd(a1; a2; ...; an)
+            lcm 	Least common multiple 	lcm(a1; a2; ...; an)
+        */
         public IReadOnlyDictionary<string, FunctionDefinition> Functions { get; } = new Dictionary<string, FunctionDefinition>()
         {
-            { nameof(Double), new FunctionDefinition("double(arg)", Double, 1, "Cast number to double.")},
-            { nameof(Int), new FunctionDefinition("int(arg)", Int, 1, "Cast number to int.")},
-            { nameof(Mean), new FunctionDefinition("mean(args...)", Mean, -1, "Mean of list of numbers.")},
-            { nameof(Math.Max), new FunctionDefinition("max(arg1, arg2)", Max, 2, "Max between two numbers.") },
-            { nameof(Math.Min), new FunctionDefinition("min(arg1, arg2)", Min, 2, "Min between two numbers.") },
-            { nameof(Round), new FunctionDefinition("round(arg, digits)", Round, 2, "Cast number to int.")},
-            { nameof(Math.Log), new FunctionDefinition("log(arg, base)", Log, 2, "Parametrable base logarithm.")},
-            { "ln", new FunctionDefinition("ln(arg)", NatualLog, 1, "Base e logarithm.")},
-            { nameof(Math.Log2), new FunctionDefinition("log2(arg)", Log2, 1, "Base 2 logarithm.")},
-            { nameof(Math.Log10), new FunctionDefinition("log10(arg)", Log10, 1, "Base 10 logarithm.")},
-            { nameof(Math.Sqrt), new FunctionDefinition("sqrt(arg)", SquareRoot, 1, "Square root.")},
-            { nameof(Math.Abs), new FunctionDefinition("abs(arg)", AbsoluteValue, 1, "Absolute value.")},
+            { "double"  , new FunctionDefinition("double(arg)",         Double,         1, "Cast number to double.")},
+            { "int"     , new FunctionDefinition("int(arg)",            Int,            1, "Cast number to int.")},
+            { "mean"    , new FunctionDefinition("mean(args...)",       Mean,           -1, "Mean of list of numbers.")},
+            { "max"     , new FunctionDefinition("max(arg1, arg2)",     Max,            2, "Max between two numbers.") },
+            { "min"     , new FunctionDefinition("min(arg1, arg2)",     Min,            2, "Min between two numbers.") },
+            { "round"   , new FunctionDefinition("round(arg, digits)",  Round,          2, "Cast number to int.")},
+            { "log"     , new FunctionDefinition("log(arg, base)",      Log,            2, "Parametrable base logarithm.")},
+            { "ln"      , new FunctionDefinition("ln(arg)",             NatualLog,      1, "Base e logarithm.")},
+            { "log2"    , new FunctionDefinition("log2(arg)",           Log2,           1, "Base 2 logarithm.")},
+            { "log10"   , new FunctionDefinition("log10(arg)",          Log10,          1, "Base 10 logarithm.")},
+            { "sqrt"    , new FunctionDefinition("sqrt(arg)",           SquareRoot,     1, "Square root.")},
+            { "abs"     , new FunctionDefinition("abs(arg)",            AbsoluteValue,  1, "Absolute value.")},
+            { "sign"    , new FunctionDefinition("sign(arg)",           Signum,         1, "Signum function. Return -1, 0 or 1 depending on the signe of the parameter.")},
+            { "floor"   , new FunctionDefinition("floor(arg)",          Floor,          1, "Round down to the nearest integer.")},
+            { "ceil"    , new FunctionDefinition("ceil(arg)",           Ceiling,        1, "Round up to the nearest integer.")},
+            { "mod"     , new FunctionDefinition("mod(arg1, arg2)",     ModuloFunc,     2, "Remainder of division.")},
+            { "fact"     , new FunctionDefinition("fact(arg)",          Factorial,      1, ".")},
         };
 
         public IReadOnlyDictionary<string, VariableDefinition> Variables { get; } = new Dictionary<string, VariableDefinition>()
@@ -37,12 +49,12 @@ namespace Tony.Calculator.Plugins
 
         public IReadOnlyDictionary<string, BinaryOperatorDefinition> BinaryOperatos { get; } = new Dictionary<string, BinaryOperatorDefinition>()
         {
-            { "+", new BinaryOperatorDefinition("+", nameof(Addition),          1, Addition) },
-            { "-", new BinaryOperatorDefinition("-", nameof(Substraction),      1, Substraction) },
-            { "*", new BinaryOperatorDefinition("*", nameof(Multiplication),    2, Multiplication) },
-            { "/", new BinaryOperatorDefinition("/", nameof(Division),          2, Division) },
-            { "%", new BinaryOperatorDefinition("%", nameof(Modulo),            2, Modulo) },
-            { "^", new BinaryOperatorDefinition("^", nameof(Exponant),          3, Exponant) },
+            { "+", new BinaryOperatorDefinition("+", "Addition",        1, Addition) },
+            { "-", new BinaryOperatorDefinition("-", "Substraction",    1, Substraction) },
+            { "*", new BinaryOperatorDefinition("*", "Multiplication",  2, Multiplication) },
+            { "/", new BinaryOperatorDefinition("/", "Division",        2, Division) },
+            { "%", new BinaryOperatorDefinition("%", "Modulo",          2, ModuloBinaryOp) },
+            { "^", new BinaryOperatorDefinition("^", "Exponant",        3, Exponant) },
         };
 
 
@@ -200,6 +212,51 @@ namespace Tony.Calculator.Plugins
             }
             throw new NotSupportedException();
         }
+
+        private static object Signum(object[] args)
+        {
+            if (args[0] is double doubleArg)
+            {
+                return Math.Sign(doubleArg);
+            }
+            if (args[0] is int intArg)
+            {
+                return Math.Sign(intArg);
+            }
+            throw new NotSupportedException();
+        }
+
+        private static object Floor(object[] args)
+        {
+            if (args[0] is double doubleArg)
+                return Math.Floor(doubleArg);
+            if (args[0] is int intArg)
+                return Math.Floor((double)intArg);
+            throw new NotSupportedException();
+        }
+
+        private static object Ceiling(object[] args)
+        {
+            if (args[0] is double doubleArg)
+                return Math.Ceiling(doubleArg);
+            if (args[0] is int intArg)
+                return Math.Ceiling((double)intArg);
+            throw new NotSupportedException();
+        }
+
+        private static object ModuloFunc(object[] args)
+        {
+            return ModuloBinaryOp(args[0], args[1]);
+        }
+
+        private static object Factorial(object[] args)
+        {
+            if (args[0] is double doubleArg)
+                return SpecialFunctions.Gamma(doubleArg + 1);
+            if (args[0] is int intArg)
+                return SpecialFunctions.Factorial(intArg);
+            throw new NotSupportedException();
+        }
         #endregion
 
         #region unary
@@ -294,7 +351,7 @@ namespace Tony.Calculator.Plugins
             throw new NotSupportedException();
         }
 
-        private static object Modulo(object arg1, object arg2)
+        private static object ModuloBinaryOp(object arg1, object arg2)
         {
             if (arg1 is double double1)
             {
